034252687111bf1893fbb4076124d11dbdfcd33b
Henrique Boaventura <hboaventura@gmail.com>
2013-06-27 21:32:49 -0300

    minify JS and update the version

--- jquery.idle.js
+++ jquery.idle.js
@@ -5,9 +5,9 @@
  *  About: Author
  *  Henrique Boaventura (hboaventura@gmail.com).
  *  About: Version
- *  1.0.0
+ *  1.1.2
  *  About: License
- *  Copyright (C) 2012, Henrique Boaventura (hboaventura@gmail.com).
+ *  Copyright (C) 2013, Henrique Boaventura (hboaventura@gmail.com).
  *  MIT License:
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *  - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
